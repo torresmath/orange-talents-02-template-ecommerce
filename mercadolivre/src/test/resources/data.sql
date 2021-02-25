03156223414a0cc536c33d28b3b672ea62a0fbeb
matheus.torres <torresmatheus_@hotmail.com>
2021-02-25 11:41:43 -0300

    hotfix data.sql syntax

--- mercadolivre/src/test/resources/data.sql
+++ mercadolivre/src/test/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO user(login, password) VALUES ('email@emuso.com', 'senhaemuso');
-INSERT INTO category(nome) VALUES ('categoria-padrao');
+INSERT INTO category(name) VALUES ('categoria-padrao');
